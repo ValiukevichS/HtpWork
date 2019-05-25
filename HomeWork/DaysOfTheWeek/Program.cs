02a79e58ba109f87008012c0f5cd51fff9e8d294
Siarhei <slon24@mail.ru>
2019-05-25 15:31:44 +0300

    All is ready

---
+++ HomeWork/DaysOfTheWeek/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace DaysOfTheWeek
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int date;
+            Console.WriteLine("Input number");
+            date = Convert.ToInt32(Console.ReadLine());
+            switch(date)
+            {
+                case 1:
+                    Console.WriteLine("Mondey");
+                    break;
+                case 2:
+                    Console.WriteLine("Tuesday");
+                    break;
+                case 3:
+                    Console.WriteLine("Wednesday");
+                    break;
+                case 4:
+                    Console.WriteLine("Wednesday");
+                    break;
+                case 5:
+                    Console.WriteLine("Friday");
+                    break;
+                case 6:
+                    Console.WriteLine("Saturday");
+                    break;
+                case 7:
+                    Console.WriteLine("Sunday");
+                    break;
+                default:                    
+                    Console.WriteLine("Enter from 1 to 7");
+                    break;
+            }
+        }
+    }
+}
---
+++ HomeWork/HomeWork/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace HomeWork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+         Console.WriteLine("Enter A Number");
+         int j = Convert.ToInt32(Console.ReadLine());
+         for (int i = 0; i <= 10; i++)
+         Console.WriteLine("{1} X {0} = {2}", i, j, i * j);
+         Console.ReadLine();
+
+        }
+    }
+}
